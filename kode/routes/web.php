ba69279b9c1bdeed5742fd684be80134df105bab
malikmadani1 <malikmadani527@gmail.com>
2024-05-30 13:02:04 +0300

    maliknew

--- kode/routes/web.php
+++ kode/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CoreController;
+//----
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FrontendController;
 use App\Http\Controllers\User\UserController;
