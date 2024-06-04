b3969afa94ed779f11816dc54f660da50f4faaa1
zaitouny <ii.zaitouny@gmail.com>
2024-06-04 15:25:41 +0300

    test 6

--- kode/resources/views/frontend/product.blade.php
+++ kode/resources/views/frontend/product.blade.php
@@ -84,7 +84,52 @@
                                         <span class="offer-tag">{{translate('off')}}  {{round($product->discount_percentage)}} %</span>
                                         @endif
                                     </div>
-                                    
+                                    <div class="product-info">
+                                            
+                                            <h4 class="product-title">
+                                                  <a href="{{route('product.details',[make_slug($product->name),$product->id])}}">
+                                                        {{$product->name}}
+                                                  </a>
+                                            </h4>
+                                            <div class="priceAndRatting">
+                                                    <div class="product-price">
+                                                        @if(($product->discount_percentage) > 0)
+                                                            <span> {{show_currency()}}{{short_amount(cal_discount($product->discount_percentage,$product->stock->first()->price))}}
+                                                            </span>  <del> {{show_currency()}}{{short_amount($product->stock->first()?$product->stock->first()->price:$product->price)}}</del>
+
+                                                            @else
+                                                            <span>
+                                                                {{show_currency()}}{{short_amount($product->stock->first()?$product->stock->first()->price:$product->price)}}
+                                                            </span>
+
+                                                        @endif
+                                                    </div>
+
+                                            </div>
+
+                                            @php
+                                                $authUser = auth_user('web');
+                                                $wishedProducts = $authUser ? $authUser->wishlist->pluck('product_id')->toArray() : [];
+                                                $randNum = "prod-".rand(6666,10000000);
+                                            @endphp
+
+                                            <div class="product-action">
+                                                <a href="javascript:void(0)" data-product_id="{{ $randNum  }}" class="buy-now addtocartbtn">
+                                                    <span class="buy-now-icon"><svg  version="1.1"  x="0" y="0" viewBox="0 0 511.997 511.997"   xml:space="preserve" ><g><path d="M405.387 362.612c-35.202 0-63.84 28.639-63.84 63.84s28.639 63.84 63.84 63.84 63.84-28.639 63.84-63.84-28.639-63.84-63.84-63.84zm0 89.376c-14.083 0-25.536-11.453-25.536-25.536s11.453-25.536 25.536-25.536c14.083 0 25.536 11.453 25.536 25.536s-11.453 25.536-25.536 25.536zM507.927 115.875a19.128 19.128 0 0 0-15.079-7.348H118.22l-17.237-72.12a19.16 19.16 0 0 0-18.629-14.702H19.152C8.574 21.704 0 30.278 0 40.856s8.574 19.152 19.152 19.152h48.085l62.244 260.443a19.153 19.153 0 0 0 18.629 14.702h298.135c8.804 0 16.477-6.001 18.59-14.543l46.604-188.329a19.185 19.185 0 0 0-3.512-16.406zM431.261 296.85H163.227l-35.853-150.019h341.003L431.261 296.85zM173.646 362.612c-35.202 0-63.84 28.639-63.84 63.84s28.639 63.84 63.84 63.84 63.84-28.639 63.84-63.84-28.639-63.84-63.84-63.84zm0 89.376c-14.083 0-25.536-11.453-25.536-25.536s11.453-25.536 25.536-25.536 25.536 11.453 25.536 25.536-11.453 25.536-25.536 25.536z" opacity="1" data-original="#000000" ></path></g></svg></span>
+                                                {{translate('Add to cart')}}
+                                                </a>
+                                                <button data-product_id ="{{$product->id}}" class="heart-btn  wishlistitem"><i class=" @if(in_array($product->id,$wishedProducts))
+                                                    fa-solid
+                                                  @else
+                                                    fa-regular
+                                                  @endif fa-heart"></i></button>
+                                            </div>
+
+                                            <form class="attribute-options-form-{{$randNum}}">
+                                                <input type="hidden" name="id" value="{{ $product->id }}">
+                                            </form>
+
+                                    </div>
                                 </div>
                             </div>
                         @empty
