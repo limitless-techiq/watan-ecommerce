6182dc13b3d85129f6616bbd44e04caaefe263d4
zaitouny <ii.zaitouny@gmail.com>
2024-06-04 17:10:43 +0300

    edit tax

--- kode/resources/views/seller/product/create.blade.php
+++ kode/resources/views/seller/product/create.blade.php
@@ -402,7 +402,9 @@
 
     $('.discount_percentage').on('keyup', function() {
         var discount = $(this).val();
+        var taxx_percentage = $("#tax_percentage").val();
         var original_price = $("#price").val();
+        original_price = original_price +(original_price*taxx_percentage/100);
         if (discount > 100) {
             $(this).val('');
             $("#dicountAmount").text('');
