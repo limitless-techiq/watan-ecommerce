a9df421a772dd7443ba0e8fa5bebe2474fe17ba7
zaitouny <ii.zaitouny@gmail.com>
2024-06-04 15:32:42 +0300

    test 7

--- kode/resources/views/frontend/product.blade.php
+++ kode/resources/views/frontend/product.blade.php
@@ -90,6 +90,21 @@
                                                         @php echo show_ratings($product->review->avg('rating')) @endphp
                                             </div>
                                             
+                                            <div class="priceAndRatting">
+                                                    <div class="product-price">
+                                                        @if(($product->discount_percentage) > 0)
+                                                            <span> {{show_currency()}}{{short_amount(cal_discount($product->discount_percentage,$product->stock->first()->price))}}
+                                                            </span>  <del> {{show_currency()}}{{short_amount($product->stock->first()?$product->stock->first()->price:$product->price)}}</del>
+
+                                                            @else
+                                                            <span>
+                                                                {{show_currency()}}{{short_amount($product->stock->first()?$product->stock->first()->price:$product->price)}}
+                                                            </span>
+
+                                                        @endif
+                                                    </div>
+
+                                            </div>
 
                                             @php
                                                 $authUser = auth_user('web');
