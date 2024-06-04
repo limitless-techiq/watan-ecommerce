be5960055bb8557df638eb32a505d9b575020401
zaitouny <ii.zaitouny@gmail.com>
2024-06-04 15:49:00 +0300

    test 15

--- kode/resources/views/frontend/product.blade.php
+++ kode/resources/views/frontend/product.blade.php
@@ -97,8 +97,8 @@
                                             <div class="priceAndRatting">
                                                     <div class="product-price">
                                                         @if(($product->discount_percentage) > 0)
-                                                            <span> {{show_currency()}}{{short_amount(cal_discount($product->price))}}
-                                                            </span>  <del> {{show_currency()}}{{short_amount($product->stock->first()?$product->stock->first()->price:$product->price)}}</del>
+                                                            <span> {{show_currency()}}{{short_amount(cal_discount($product->discount_percentage,$product->stock->first()->price))}}
+                                                            </span>  <del> 12</del>
 
                                                             @else
                                                             <span>
