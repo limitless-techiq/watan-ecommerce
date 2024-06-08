cad8e8ae30d876c205e056be2b42dcf31194c210
zaitouny <ii.zaitouny@gmail.com>
2024-06-08 23:04:19 +0300

    optimize price1

--- kode/resources/views/frontend/partials/product.blade.php
+++ kode/resources/views/frontend/partials/product.blade.php
@@ -70,6 +70,7 @@
                     {{ show_currency() }}&nbsp;{{ number_format(short_amount(cal_discount($product->discount_percentage,
                     $product->stock->first()->price)), 2, '.', ',') }} 
                 </span>
+                <br>
                 <del>
                     
                     {{ show_currency() }}&nbsp;{{ number_format(short_amount($product->stock->first() ?
