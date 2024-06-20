b586ecfff26730b79d23003ad88e6c64963ccc4b
zaitouny <ii.zaitouny@gmail.com>
2024-06-20 17:36:50 +0300

    test 1

--- kode/resources/views/frontend/section/best_selling_product.blade.php
+++ kode/resources/views/frontend/section/best_selling_product.blade.php
@@ -68,12 +68,13 @@
                                 </div>
                                 <div class="product-price">
                                     @if(($product->discount_percentage) > 0)
+
                                     <span>
                                         {{ show_currency() }}&nbsp;{{
-                                            number_format(cal_discount($product->discount_percentage,
-                                            $product->stock->first() ? $product->stock->first()->price : $product->price),  2, '.', ',') }}
-
+                                        number_format(short_amount(cal_discount($product->discount_percentage,
+                                        $product->stock->first()->price)), 2, '.', ',') }}
                                     </span>
+                                    
 
                                     <del>
                                         {{ show_currency() }}&nbsp;{{ number_format(short_amount($product->stock->first() ?
--- kode/resources/views/frontend/section/top_product.blade.php
+++ kode/resources/views/frontend/section/top_product.blade.php
@@ -44,11 +44,11 @@
                                     @if(($product->discount_percentage) > 0)
                                     <span>
                                         {{ show_currency() }}&nbsp;{{
-                                        number_format(cal_discount($product->discount_percentage,
-                                        $product->stock->first() ? $product->stock->first()->price : $product->price), 2, '.', ',') }}
-
-
-                                    </span> <del>
+                                        number_format(short_amount(cal_discount($product->discount_percentage,
+                                        $product->stock->first()->price)), 2, '.', ',') }}
+                                    </span>
+                                    
+                                    <del>
                                         {{ show_currency() }}&nbsp;{{ number_format(short_amount($product->stock->first() ?
                                         $product->stock->first()->price : $product->price),  2, '.', ',') }}
                                     </del>
