089f5de5e550d4f14ab39f64ddafb68bc713b627
zaitouny <ii.zaitouny@gmail.com>
2024-06-20 17:34:04 +0300

    opt best selling product and top product

--- kode/resources/views/frontend/section/best_selling_product.blade.php
+++ kode/resources/views/frontend/section/best_selling_product.blade.php
@@ -71,20 +71,19 @@
                                     <span>
                                         {{ show_currency() }}&nbsp;{{
                                             number_format(cal_discount($product->discount_percentage,
-                                            $product->stock->first() ? $product->stock->first()->price : $product->price),
-                                            2) }}
+                                            $product->stock->first() ? $product->stock->first()->price : $product->price),  2, '.', ',') }}
 
                                     </span>
 
                                     <del>
                                         {{ show_currency() }}&nbsp;{{ number_format(short_amount($product->stock->first() ?
-                                        $product->stock->first()->price : $product->price), 2) }}
+                                        $product->stock->first()->price : $product->price), 2, '.', ',') }}
                                     </del>
                                     
 
                                     @else
                                     <span>
-                                        {{show_currency()}}&nbsp;{{short_amount($product->stock->first()?$product->stock->first()->price:$product->price)}}
+                                        {{show_currency()}}&nbsp;{{number_format(short_amount($product->stock->first()?$product->stock->first()->price:$product->price),  2, '.', ',')}}
                                     </span>
 
                                     @endif
--- kode/resources/views/frontend/section/top_product.blade.php
+++ kode/resources/views/frontend/section/top_product.blade.php
@@ -45,18 +45,17 @@
                                     <span>
                                         {{ show_currency() }}&nbsp;{{
                                         number_format(cal_discount($product->discount_percentage,
-                                        $product->stock->first() ? $product->stock->first()->price : $product->price),
-                                        2) }}
+                                        $product->stock->first() ? $product->stock->first()->price : $product->price), 2, '.', ',') }}
 
 
                                     </span> <del>
                                         {{ show_currency() }}&nbsp;{{ number_format(short_amount($product->stock->first() ?
-                                        $product->stock->first()->price : $product->price), 2) }}
+                                        $product->stock->first()->price : $product->price),  2, '.', ',') }}
                                     </del>
 
                                     @else
                                     <span>
-                                        {{show_currency()}}&nbsp;{{round(short_amount($product->stock->first()?$product->stock->first()->price:$product->price))}}
+                                        {{show_currency()}}&nbsp;{{number_format(short_amount($product->stock->first()?$product->stock->first()->price:$product->price),  2, '.', ',')}}
                                     </span>
 
                                     @endif
