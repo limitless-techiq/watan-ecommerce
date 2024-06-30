574244afccdd0129a43c7784709dc2805eff935a
zaitouny <ii.zaitouny@gmail.com>
2024-06-30 03:20:59 +0300

    tax6

--- kode/resources/views/frontend/partials/script.blade.php
+++ kode/resources/views/frontend/partials/script.blade.php
@@ -570,8 +570,8 @@
         }
         var subtotalamount = $('#subtotalamount').attr('data-sub');
         var value = $(this).data('shipping_price');
-        var totalamount = parseFloat(subtotalamount) ;
-        var aftertotalamount = parseFloat(totalamount) - parseFloat(couponamount);
+        var totalamount = parseFloat(subtotalamount) ;///+ parseFloat(value)
+        var aftertotalamount = parseFloat(totalamount+($totalamount*$parseFloat(value)/100)) - parseFloat(couponamount);
         $('#shipping_cost').text(`${value}`);
         $('#totalamount').text(parseFloat(aftertotalamount).toFixed(2));
         $('.order-shipping-cost').removeClass('d-none');
