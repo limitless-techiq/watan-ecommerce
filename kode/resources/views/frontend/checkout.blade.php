c5450409b054444137b2ff2da7b9dd07613340d8
zaitouny <ii.zaitouny@gmail.com>
2024-06-30 03:33:36 +0300

    tax9

--- kode/resources/views/frontend/checkout.blade.php
+++ kode/resources/views/frontend/checkout.blade.php
@@ -558,7 +558,7 @@
                                 </li> --}}
 
                                 <li class="order-cost-item order-shipping-cost d-none d-flex align-items-center justify-content-between gap-4">
-                                    <span class="ps-4 py-3 nowrap fs-14">{{translate("Tax and shipping Charge")}} {{number_format($tax, 2)}} %:</span>
+                                    <span class="ps-4 py-3 nowrap fs-14">{{translate("Tax and shipping Charge")}} %<span id="shipping_cost"></span> :</span>
                                     <span class="text-end pe-4 py-3 nowrap fs-14" >
                                         {{show_currency()}}&nbsp;{{number_format(($subTotal*$tax/100), 2)}}
                                     </span>
